ac1cd9922cf72bb71191915cc6c84733c9908198
ccathala <cathala.cha@gmail.com>
2022-08-30 09:45:40 +0200

    Init project

---
+++ src/main/java/com/ccathala/mesdepensesapi/MesdepensesApiApplication.java
@@ -0,0 +1,15 @@
+package com.ccathala.mesdepensesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesdepensesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesdepensesApiApplication.class, args);
+		System.out.println("Coucou");
+		System.out.println("Recoucou");
+	}
+
+}
