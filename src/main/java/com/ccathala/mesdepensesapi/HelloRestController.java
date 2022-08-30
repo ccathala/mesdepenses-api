1fca8223a4bbb2df5909d17ab367f9af75f34731
ccathala <cathala.cha@gmail.com>
2022-08-30 13:24:05 +0200

    Add Hello

---
+++ src/main/java/com/ccathala/mesdepensesapi/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.ccathala.mesdepensesapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    
+    @GetMapping(value = "/hello")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String hello() {
+        return "Hello";
+    }
+}
